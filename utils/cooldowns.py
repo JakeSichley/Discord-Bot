6b78ff50b49754cad0601d12228b8cccb901e0a3
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2022-11-21 17:23:51 -0800

    More planning

--- utils/cooldowns.py
+++ utils/cooldowns.py
@@ -14,6 +14,7 @@ async def on_test_error(interaction: discord.Interaction, error: app_commands.Ap
     if isinstance(error, app_commands.CommandOnCooldown):
         await interaction.response.send_message(str(error), ephemeral=True)
 """
+import discord
 
 """ --- Discord Example --- 
 class CooldownModified:
@@ -41,30 +42,40 @@ Access bot from ctx or itx
 self.bot.dynamic_cooldowns[qualified_command_name, ExponentialCooldown]
 
 Don't need to initial actual rate in ExponentialCooldown() unless default cooldown is desired
-
 """
 
 from dataclasses import dataclass
-from discord.ext.commands import Cooldown
-from typing import TypeVar, Hashable, Optional, Dict
+from discord.ext import commands
+from typing import TypeVar, Hashable, Optional, Dict, Protocol
 from datetime import datetime, timedelta
-
-T = TypeVar('T', bound=Hashable)
-
-
+import discord
 
 
 @dataclass
 class CooldownMapping:
-    # key: T
     count: int = 0
     expires: Optional[datetime] = None
 
+    # this needs to have actual cooldown logic in it
+    #  maybe have some fancy __get__ logic to expire cooldowns automatically
+
+    """ --- Cases ---
+        - User repeatedly fails
+            - Need to know to increase cooldown after current cooldown ends
+                - If now() > expires_at, no cooldown
+            - When to reset cooldown for user who fails
+                - User fails once, then succeeds, reset to 0? Decrement failure_count by amount?
+    """
 
 
-class ExponentialCooldown:
+
+class ExponentialCommandCooldown:
     def __init__(self, rate: float, per: float) -> None:
-        self.rate = rate
-        self.per = per
-        self.mapping: Dict[T, CooldownMapping] = {}
+        self.__rate = rate
+        self.__per = per
+        self.__mapping: Dict[int, CooldownMapping] = {}
 
+    async def __call__(self, message: discord.Message) -> Optional[commands.Cooldown]:
+        # call method doesn't ever directly modify mapping, only checks
+
+        cooldown = self.__mapping.get(message.author.id, CooldownMapping())
