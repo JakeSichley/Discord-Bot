469c2a0c69e3d5ea66f59d5be95cd4e82adaa6b2
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2023-03-20 18:27:11 -0700

    Changelog, Version Bump

--- utils/utils.py
+++ utils/utils.py
@@ -40,7 +40,7 @@ from fuzzywuzzy import fuzz  # type: ignore
 
 from utils.logging_formatter import bot_logger
 
-VERSION = '2.10.1'
+VERSION = '2.10.2'
 
 ChoiceT = TypeVar('ChoiceT', str, int, float, Union[str, int, float])
 
