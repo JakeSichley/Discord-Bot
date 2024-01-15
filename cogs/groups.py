c52e03c9ff605a8fb68f78b2d4bcc750f72ac89b
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2024-01-15 05:15:21 -0800

    Data refresh task in place of secondary cache

--- cogs/groups.py
+++ cogs/groups.py
@@ -41,6 +41,8 @@ from utils.intermediate_models.composite_group import CompositeGroup
 from utils.logging_formatter import bot_logger
 from utils.utils import format_unix_dt, generate_autocomplete_choices
 
+from discord.ext import tasks
+
 
 # TODO: Groups v3 -> edit group; needs components for confirmation when new max_members < current_members
 # TODO: Groups v3 -> Cache member_id -> groups for leave/join event cache syncing?
@@ -53,7 +55,10 @@ class Groups(commands.GroupCog, group_name='group', group_description='Commands 
 
     Attributes:
         bot (DreamBot): The Discord bot class.
+        GROUPS_REFRESH_INTERVAL (int): How frequently group data should be refreshed from the database.
     """
+
+    GROUPS_REFRESH_INTERVAL = 60 * 15  # 15 minutes
 
     def __init__(self, bot: DreamBot) -> None:
         """
@@ -66,35 +71,7 @@ class Groups(commands.GroupCog, group_name='group', group_description='Commands 
         self.bot = bot
         # [guild_id: [group_name: CompositeGroup]]
         self.groups: Dict[int, Dict[str, CompositeGroup]] = defaultdict(dict)
-
-    async def cog_load(self) -> None:
-        """
-        A special method that acts as a cog local post-invoke hook.
-
-        Parameters:
-            None.
-
-        Returns:
-            None.
-        """
-
-        groups = await typed_retrieve_query(
-            self.bot.database,
-            Group,
-            'SELECT * FROM GROUPS'
-        )
-
-        for group in groups:
-            self.groups[group.guild_id][group.group_name] = CompositeGroup(group)
-
-        group_members = await typed_retrieve_query(
-            self.bot.database,
-            GroupMember,
-            'SELECT * FROM GROUP_MEMBERS'
-        )
-
-        for group_member in group_members:
-            self.groups[group_member.guild_id][group_member.group_name].members.add(group_member.member_id)
+        self.group_data_refresh.start()
 
     @app_commands.checks.cooldown(1, 10.0, key=lambda i: (i.guild_id, i.user.id))  # type: ignore[arg-type]
     @app_commands.command(  # type: ignore[arg-type]
@@ -679,6 +656,61 @@ class Groups(commands.GroupCog, group_name='group', group_description='Commands 
                 'DELETE FROM GROUP_MEMBERS WHERE GUILD_ID=? AND MEMBER_ID=?',
                 (payload.guild_id, payload.user.id)
             )
+
+    """
+    MARK: - Tasks
+    """
+
+    @tasks.loop(seconds=GROUPS_REFRESH_INTERVAL)
+    async def group_data_refresh(self) -> None:
+        """
+        A discord.ext.tasks loop that refreshes group data.
+        Executes every {GROUPS_REFRESH_INTERVAL} seconds.
+
+        TODO: Remove when member -> group mapping is implemented; move back to cog_load
+
+        Parameters:
+            None.
+
+        Returns:
+            None.
+        """
+
+        print('Refreshed')
+
+        groups = await typed_retrieve_query(
+            self.bot.database,
+            Group,
+            'SELECT * FROM GROUPS'
+        )
+
+        for group in groups:
+            self.groups[group.guild_id][group.group_name] = CompositeGroup(group)
+
+        group_members = await typed_retrieve_query(
+            self.bot.database,
+            GroupMember,
+            'SELECT * FROM GROUP_MEMBERS'
+        )
+
+        for group_member in group_members:
+            self.groups[group_member.guild_id][group_member.group_name].members.add(group_member.member_id)
+
+    async def cog_unload(self) -> None:
+        """
+        A method detailing custom extension unloading procedures.
+        Clears internal caches and immediately and forcefully exits any discord.ext.tasks.
+
+        Parameters:
+            None.
+
+        Returns:
+            None.
+        """
+
+        self.group_data_refresh.cancel()
+
+        bot_logger.info('Completed Unload for Cog: Groups')
 
 
 def calculate_member_and_joined_max_splice(group_members: List[Tuple[discord.Member, int]]) -> int:
