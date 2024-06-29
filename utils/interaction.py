b561d3a6515265f8d4621a231b23de3ea121f23f
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2024-06-29 01:58:58 -0700

    Create GuildInteraction for type checking

---
+++ utils/interaction.py
@@ -0,0 +1,41 @@
+"""
+MIT License
+
+Copyright (c) 2019-Present Jake Sichley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import Union
+
+from discord import Interaction, Member
+from discord.channel import VoiceChannel, StageChannel, TextChannel, ForumChannel, CategoryChannel
+from discord.threads import Thread
+
+from dreambot import DreamBot
+
+
+class GuildInteraction(Interaction[DreamBot]):
+    """
+    An Interaction subclass for guild-only invocations, primarily used for type checking.
+    """
+
+    guild_id: int
+    channel: Union[VoiceChannel, StageChannel, TextChannel, ForumChannel, CategoryChannel, Thread]
+    user: Member
