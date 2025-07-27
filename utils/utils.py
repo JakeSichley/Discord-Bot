b5eb09aa5f55f3b5b6b8cc8cdc90ce0a993cb87f
DREAM-V2\Jake <39101429+JakeSichley@users.noreply.github.com>
2025-07-26 18:55:57 -0700

    Changelog, version bump

--- utils/utils.py
+++ utils/utils.py
@@ -40,7 +40,7 @@ from fuzzywuzzy import fuzz  # type: ignore
 
 from utils.logging_formatter import bot_logger
 
-VERSION = '2.18.4'
+VERSION = '2.18.5'
 
 ChoiceT = TypeVar('ChoiceT', str, int, float, Union[str, int, float])
 
